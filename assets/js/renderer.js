3170ac4f32207d01d0e4a47bb9282668abd87072
salar500 <baavanandu@gmail.com>
2026-08-03 23:31:14 +0530

    Create renderer.js

---
+++ assets/js/renderer.js
@@ -0,0 +1,15 @@
+function renderHeader(){
+
+    document
+        .getElementById("header")
+        .innerHTML = Components.header;
+
+}
+
+function renderFooter(){
+
+    document
+        .getElementById("footer")
+        .innerHTML = Components.footer;
+
+}
