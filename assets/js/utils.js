d3e6bfe264fbff016f1485f8bf1612fa7b8db65a
salar500 <baavanandu@gmail.com>
2026-08-03 23:32:04 +0530

    Add currency and percentage utility functions

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,18 @@
+function currency(value){
 
+    return new Intl.NumberFormat(
+        "en-IN",
+        {
+            style:"currency",
+            currency:"INR",
+            maximumFractionDigits:0
+        }
+    ).format(value);
+
+}
+
+function percentage(value){
+
+    return value+"%";
+
+}
