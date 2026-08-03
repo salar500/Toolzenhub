f2284e6ce794660e0dd7d9a925f479f7339dd934
salar500 <baavanandu@gmail.com>
2026-08-03 23:29:49 +0530

    Update components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,1 +1,45 @@
+const Components = {
 
+header: `
+<header class="header">
+
+    <div class="container">
+
+        <nav class="navbar">
+
+            <a href="/" class="logo">
+                ToolZen Hub
+            </a>
+
+            <ul class="nav-links">
+
+                <li><a href="/">Home</a></li>
+
+                <li><a href="/categories/">Categories</a></li>
+
+                <li><a href="/articles/">Articles</a></li>
+
+                <li><a href="/about/">About</a></li>
+
+            </ul>
+
+        </nav>
+
+    </div>
+
+</header>
+`,
+
+footer: `
+<div class="container">
+
+    <p>
+
+        © 2026 ToolZen Hub
+
+    </p>
+
+</div>
+`
+
+}
