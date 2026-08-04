4a3a7a1c60cb58305cb4f750ee493e737e96d403
salar500 <baavanandu@gmail.com>
2026-08-05 00:10:35 +0530

    Update formulaExecutor.js

--- assets/js/calculators/engine/formulaExecutor.js
+++ assets/js/calculators/engine/formulaExecutor.js
@@ -1,1 +1,21 @@
+// assets/js/calculators/engine/formulaExecutor.js
 
+import * as EMI from "../formulas/emi.js";
+
+const formulas = {
+
+    emi: EMI.calculate
+
+};
+
+export function executeFormula(name, values) {
+
+    if (!formulas[name]) {
+
+        throw new Error(`Formula "${name}" not found`);
+
+    }
+
+    return formulas[name](values);
+
+}
