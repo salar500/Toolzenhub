7e5429a506b9a76164501f28648ec229eae7c356
salar500 <baavanandu@gmail.com>
2026-08-05 00:17:22 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,19 @@
+// assets/js/core/config.js
 
+export const CONFIG = {
+  APP_NAME: "ToolZen Hub",
+  VERSION: "1.0.0",
+
+  PATHS: {
+    DATA: "/data/",
+    IMAGES: "/assets/images/",
+    ICONS: "/assets/icons/"
+  },
+
+  FEATURES: {
+    DARK_MODE: false,
+    SEARCH: true,
+    ANALYTICS: false,
+    PWA: false
+  }
+};
