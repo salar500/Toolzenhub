bf51b5425b4ff0aa16daa0c13358b2e3f51e5b10
salar500 <baavanandu@gmail.com>
2026-08-05 00:18:59 +0530

    Update router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,1 +1,15 @@
+// assets/js/core/router.js
 
+export function currentPage() {
+
+    const path = window.location.pathname;
+
+    if (path === "/") {
+
+        return "home";
+
+    }
+
+    return path;
+
+}
