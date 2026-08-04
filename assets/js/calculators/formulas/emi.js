30eef391327bc1672cfa24bd2f3c0d68fe039a70
salar500 <baavanandu@gmail.com>
2026-08-05 00:12:08 +0530

    Update emi.js

--- assets/js/calculators/formulas/emi.js
+++ assets/js/calculators/formulas/emi.js
@@ -1,1 +1,27 @@
+// assets/js/calculators/formulas/emi.js
 
+export function calculate(values) {
+
+    const P = values.loan;
+
+    const r = values.rate / 1200;
+
+    const n = values.years * 12;
+
+    const emi =
+        P *
+        r *
+        Math.pow(1 + r, n) /
+        (Math.pow(1 + r, n) - 1);
+
+    return {
+
+        monthlyEmi: Math.round(emi),
+
+        totalPayment: Math.round(emi * n),
+
+        totalInterest: Math.round((emi * n) - P)
+
+    };
+
+}
