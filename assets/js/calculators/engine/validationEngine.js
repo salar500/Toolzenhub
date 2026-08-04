02ceb25c9242b6f236dd7aa7f7da33e7b1f386b0
salar500 <baavanandu@gmail.com>
2026-08-05 00:11:14 +0530

    Update validationEngine.js

--- assets/js/calculators/engine/validationEngine.js
+++ assets/js/calculators/engine/validationEngine.js
@@ -1,1 +1,31 @@
+// assets/js/calculators/engine/validationEngine.js
 
+export function validateInputs(schema, values) {
+
+    for (const input of schema) {
+
+        const value = values[input.id];
+
+        if (value === undefined) {
+
+            return { valid: false };
+
+        }
+
+        if (input.min !== undefined && value < input.min) {
+
+            return { valid: false };
+
+        }
+
+        if (input.max !== undefined && value > input.max) {
+
+            return { valid: false };
+
+        }
+
+    }
+
+    return { valid: true };
+
+}
