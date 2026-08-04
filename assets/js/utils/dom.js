6f828be15fd217c90e09ddc4f79e1b6f2ef8959d
salar500 <baavanandu@gmail.com>
2026-08-05 00:17:40 +0530

    Add utility functions for DOM manipulation

--- assets/js/utils/dom.js
+++ assets/js/utils/dom.js
@@ -1,1 +1,17 @@
+// assets/js/utils/dom.js
 
+export const $ = (selector, parent = document) =>
+  parent.querySelector(selector);
+
+export const $$ = (selector, parent = document) =>
+  [...parent.querySelectorAll(selector)];
+
+export function create(tag, className = "") {
+  const element = document.createElement(tag);
+
+  if (className) {
+    element.className = className;
+  }
+
+  return element;
+}
