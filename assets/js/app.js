aa9e9f9b3a8807364b1466319489818dad3bb3c1
salar500 <baavanandu@gmail.com>
2026-08-10 14:59:40 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,21 @@
-/* =========================================================
-   ToolZen Hub
-   Application Entry
-========================================================= */
-
 import { renderHeader } from "./components/header.js";
 import { renderHero } from "./components/hero.js";
 import { renderCategories } from "./components/categories.js";
+import { renderArticles } from "./components/articles.js";
 
-
-/* =========================================================
-   Application Initialization
-========================================================= */
 
 function initializeApp() {
 
-    /* Header */
     renderHeader();
 
-
-    /* Hero */
     renderHero();
 
-
-    /* Categories + Popular Calculators */
     renderCategories();
+
+    renderArticles();
 
 }
 
-
-/* =========================================================
-   DOM Ready
-========================================================= */
 
 document.addEventListener(
     "DOMContentLoaded",
