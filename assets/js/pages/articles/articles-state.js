f91aba91afd9f42d52589528cf5732633058fe61
salar500 <baavanandu@gmail.com>
2026-08-25 11:17:46 +0530

    Update articles-state.js

--- assets/js/pages/articles/articles-state.js
+++ assets/js/pages/articles/articles-state.js
@@ -1,1 +1,18 @@
+/* =========================================================
+   ToolZen Hub
+   Articles State
+========================================================= */
 
+export const articlesState = {
+
+    selectedCategory: "All",
+
+    currentPage: 1,
+
+    searchTerm: "",
+
+    articlesPerPage: 5,
+
+    categoriesExpanded: false
+
+};
