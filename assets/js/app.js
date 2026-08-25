bbaf11b731a9f149fa7b713445760bdb23b2c0fa
salar500 <baavanandu@gmail.com>
2026-08-25 12:12:31 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,7 +36,7 @@ import { renderArticlesPage }
 
 
 /* =========================================================
-   Application
+   APPLICATION
 ========================================================= */
 
 async function initializeApp() {
