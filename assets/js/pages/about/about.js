41e24dbbac4812830395b239b869b12b76e4235e
salar500 <baavanandu@gmail.com>
2026-08-26 16:23:25 +0530

    Update about.js

--- assets/js/pages/about/about.js
+++ assets/js/pages/about/about.js
@@ -1,1 +1,16 @@
+import {
+    renderAboutTemplate
+} from "./about-template.js";
 
+
+export function renderAboutPage() {
+
+    const app = document.getElementById("app");
+
+    if (!app) {
+        return;
+    }
+
+    app.innerHTML = renderAboutTemplate();
+
+}
