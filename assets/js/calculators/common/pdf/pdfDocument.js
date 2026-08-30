316c8b1a616c4239ee290c0fbdf838686bed989c
salar500 <baavanandu@gmail.com>
2026-08-30 22:15:11 +0530

    Update pdfDocument.js

--- assets/js/calculators/common/pdf/pdfDocument.js
+++ assets/js/calculators/common/pdf/pdfDocument.js
@@ -1,44 +1,41 @@
-
 /* =========================================================
    ToolZen Hub
    PDF Document
 ========================================================= */
 
+import {
+    loadPdfLibraries
+} from "./pdfLoader.js";
 
-/*
- * jsPDF is loaded globally.
- *
- * Example:
- *
- * <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
- *
- */
 
+/* =========================================================
+   CREATE PDF DOCUMENT
+========================================================= */
 
 export async function createPDFDocument() {
 
-    if (
-        !window.jspdf ||
-        !window.jspdf.jsPDF
-    ) {
-
-        throw new Error(
-            "jsPDF library is not loaded."
-        );
-
-    }
-
-
+    /*
+     * Load jsPDF + AutoTable
+     * only when PDF generation is requested.
+     */
     const {
         jsPDF
-    } = window.jspdf;
+    } =
+        await loadPdfLibraries();
 
 
+    /*
+     * Create document
+     */
     const doc =
         new jsPDF({
+
             orientation: "portrait",
+
             unit: "mm",
+
             format: "a4"
+
         });
 
 
